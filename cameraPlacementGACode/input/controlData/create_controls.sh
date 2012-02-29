f23c8126c1706da0ff149a6a4e958a0913ef00fd
Aaron P Wagner <apwagner@msu.edu>
2012-02-29 18:21:14 -0500

    Added to and revised control data generation scripts. M115 control data generated for 30 trials for all 4 control types.

--- cameraPlacementGACode/input/controlData/create_controls.sh
+++ cameraPlacementGACode/input/controlData/create_controls.sh
@@ -4,9 +4,16 @@
 # scenario 0 only has 1 control b/c it is not stochastic (but create multiple anyway to ease testing by week)
 # scenario 3 requires a file of locations to pull opportunistic locations from
 
+# i = num control types
+# j = number of replicates
+# k = number of weeks of data 
+
 for ((i=0; i<4; i++)); do
-	for ((j=0; j<30; j++)); do
-      python placeControlCams.py -s ${i} -o M115/c_${i}_${j}	-l ../fieldData/hyenaData/M115/M115_${j}_${j}
+  for ((j=0; j<30; j++)); do
+      mkdir M115/rep${j}
+	for ((k=0; k<25; k++)); do
+      python placeControlCams.py -s ${i} -o M115/rep${j}/c_${i}_${k} -l ../fieldData/hyenaData/M115/M115_${k}_${k}
 	done;
+  done;
 done;
 
