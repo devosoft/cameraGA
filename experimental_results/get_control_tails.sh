2c6437987c21e0f344bc65982ad731be8ec57715
Aaron P Wagner <apwagner@msu.edu>
2012-03-06 20:25:54 -0500

    Updated get_control_tails script.

--- experimental_results/get_control_tails.sh
+++ experimental_results/get_control_tails.sh
@@ -3,13 +3,16 @@
 # i = num control types
 # j = number of replicates
 # k = number of weeks of data 
+# l = fitness function
 
-mkdir M115/control_results
+mkdir control_results/M115
+l=0
+mkdir control_results/M115/fit_func${l}
 for ((i=0;i<4;i++)); do
   for ((j=0;j<30;j++)); do
     for ((k=0;k<24;k++)); do
-      tail -n 1 M115/control_tests/rep${j}/control_M115_${i}_${k} >> M115/control_results/control_${i}_wk${k}.dat
-      echo >> M115/control_results/control_${i}_wk${k}.dat
+      tail -n 1 fit_func${l}/M115/control_tests/rep${j}/control_M115_${i}_${k} >> control_results/M115/fit_func${l}/control_${i}_wk${k}.dat
+      echo >> control_results/M115/fit_func${l}/control_${i}_wk${k}.dat
     done
   done
 done
